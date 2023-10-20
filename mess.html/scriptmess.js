45a2dead958c626ff32cbfa1e5ea52aabb9c8c70
NullyIsHere <140397732+NullyIsHere@users.noreply.github.com>
2023-10-20 17:08:48 +0000

    Mino Changes

--- mess.html/scriptmess.js
+++ mess.html/scriptmess.js
@@ -1,30 +1,22 @@
 var typewriter = {
   content: [
-    "T'envio tot el meu suport",
-    "Segueix lluitant, no et rendeixis",
-    "Ets més fort del que penses",
-    "La teva força és com un foc",
-    "Fes-ho amb passió o no ho facis",
-    "Cada dia és una nova oportunitat",
-    "Ets capaç de conquerir el món",
-    "Recorda: ets increïble tal com ets",
-    "La vida és bella, gaudeix-la plenament",
-    "La felicitat està en les petites coses",
-    "No estàs sol, sempre hi ha algú per tu",
-    "Cada dificultat és una oportunitat de créixer",
-    "Estàs fent un gran progrés, segueix endavant",
-    "El teu somriure il·umina el dia de molta gent",
-    "Tu ets únic i especial",
-    "Confia en tu mateix, ets formidable"
+    "Mira cap al futur amb esperança i determinació.",
+    "Anima't a seguir endavant cada dia.",
+    "Recull forces i mantén la calma en tot moment.",
+    "Troba la teva pròpia inspiració i enfoca't en els teus objectius.",
+    "Imagina les possibilitats infinites que tens davant teu.",
+    "No et rendeixis davant dels desafiaments de la vida.",
+    "Aprofita totes les oportunitats que et vinguin.",
+
   ],
   part: 0,
   partIndex: 0,
-  intervalVal: null,
   element: document.querySelector("#text"),
   cursor: document.querySelector("#cursor"),
   typingSpeed: 100,
   displayDuration: 3000,
   restartPageAfterCompletion: true, // Recargar la página al completar
+  intervalVal: null,
 
   type: function () {
     var text = typewriter.content[typewriter.part].substring(0, typewriter.partIndex + 1);
@@ -62,8 +54,35 @@ var typewriter = {
         typewriter.intervalVal = setInterval(typewriter.type, typewriter.typingSpeed);
       }, 200);
     }
-  }
+  },
+
+  // Add a function to get the announcement from the backend
+  getAnnouncement: function () {
+    var announcementContainer = document.querySelector("#last-announcement");
+
+    // Make an AJAX request to get the announcement
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/get_announcement", true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        var announcement = response.announcement;
+        announcementContainer.innerHTML = announcement;
+      }
+    };
+
+    xhr.send();
+  },
 };
 
+// Call the getAnnouncement function to fetch the announcement initially
+typewriter.getAnnouncement();
+
+// Set an interval to update the announcement and the typewriter every 5 seconds
+setInterval(function () {
+  typewriter.getAnnouncement();
+}, 5000);
+
+// Initialize the typewriter
 typewriter.intervalVal = setInterval(typewriter.type, typewriter.typingSpeed);
-  
